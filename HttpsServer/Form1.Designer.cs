e09e1e758e539ac0021c5edc78779cd2f743fa43
Marijo Horvat <Marijo.Horvat@vsite.hr>
2017-03-28 17:25:18 +0200

    Umm, did i forget to commit?

--- HttpsServer/Form1.Designer.cs
+++ HttpsServer/Form1.Designer.cs
@@ -28,12 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ListenerLabel = new System.Windows.Forms.Label();
+            this.ListenerTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ListenerLabel
+            // 
+            this.ListenerLabel.AutoSize = true;
+            this.ListenerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ListenerLabel.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ListenerLabel.Location = new System.Drawing.Point(12, 9);
+            this.ListenerLabel.Name = "ListenerLabel";
+            this.ListenerLabel.Size = new System.Drawing.Size(131, 18);
+            this.ListenerLabel.TabIndex = 0;
+            this.ListenerLabel.Text = "Listener Status::";
+            // 
+            // ListenerTextBox
+            // 
+            this.ListenerTextBox.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ListenerTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ListenerTextBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.ListenerTextBox.Location = new System.Drawing.Point(12, 39);
+            this.ListenerTextBox.Multiline = true;
+            this.ListenerTextBox.Name = "ListenerTextBox";
+            this.ListenerTextBox.Size = new System.Drawing.Size(376, 127);
+            this.ListenerTextBox.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(709, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(841, 380);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ListenerTextBox);
+            this.Controls.Add(this.ListenerLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label ListenerLabel;
+        private System.Windows.Forms.TextBox ListenerTextBox;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- HttpsServer/Form1.cs
+++ HttpsServer/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
 
 namespace HttpsServer
 {
@@ -16,5 +20,50 @@ namespace HttpsServer
         {
             InitializeComponent();
         }
+
+         private void StartListener()
+        {
+            HttpListener web = new HttpListener();
+            web.Prefixes.Add("http://*:8080/");
+            //web.Prefixes.Add("https://*:8443/");
+            SetText("Listening ... ");
+            web.Start();
+            SetText("Starting GetContext() ...");
+            HttpListenerContext context = web.GetContext();
+            SetText("GetContext() got something! ");
+            HttpListenerResponse response = context.Response;
+            const string responseString = "<html><body>Hello world</body></html>";
+            var buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            response.ContentLength64 = buffer.Length;
+            Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            SetText("Output displayed!");
+            output.Close();
+            web.Stop();
+        }
+        private void SetText(string text)
+        {
+
+            if (this.ListenerTextBox.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.ListenerTextBox.AppendText(text + Environment.NewLine);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread ListenerThread = new Thread(new ThreadStart(StartListener));
+            ListenerThread.Start();
+        }
+
+
+
+
+        delegate void SetTextCallback(string text);
     }
 }
